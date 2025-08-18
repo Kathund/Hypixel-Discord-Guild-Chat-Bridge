386c6edd4ce3b73b3dac0fe2b53edd14b30d8dc4
Jacob <kathund@proton.me>
2025-08-18 11:09:28 +0800

    remove(MiscConfig): Test values

--- src/Config/Configs/MiscConfig.ts
+++ src/Config/Configs/MiscConfig.ts
@@ -1,7 +1,4 @@
-import BooleanOption from '../BooleanConfigOption';
 import ConfigInstance from '../Private/ConfigInstance';
-import NumberOption from '../NumberConfigOption';
-import StringOption from '../StringConfigOption';
 import StringSelectionOption from '../StringSelectionConfigOption';
 // eslint-disable-next-line import/no-cycle
 import { getSupportedLanguages } from '../../Private/Translate';
@@ -11,9 +8,6 @@ class MiscConfig extends ConfigInstance {
     super('misc', update);
     this.updateData();
     this.setValue('lang', new StringSelectionOption('en_us', getSupportedLanguages()), false);
-    this.setValue('boolean', new BooleanOption(false), false);
-    this.setValue('number', new NumberOption(1), false);
-    this.setValue('string', new StringOption('fuck'), false);
   }
 }
 
