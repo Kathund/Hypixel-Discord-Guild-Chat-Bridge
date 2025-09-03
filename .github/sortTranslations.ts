612acfde619d7d3b4f9e7c2b4afbbaaa895aaf00
Jacob <kathund@proton.me>
2025-09-03 09:50:58 +0800

    refactor(translations parse): Make it more useful and better for supporting multiple langs

--- .github/sortTranslations.ts
+++ .github/sortTranslations.ts
@@ -3,23 +3,61 @@ import { sortJSON } from '../src/Utils/JSONUtils';
 import { writeFileSync } from 'node:fs';
 import type { Language } from '../src/types/main';
 
-function findTranslations(lang: Language) {
-  const translations = getTranslations(lang);
-  translations['!!'] = 'DO NOT TRANSLATE ANYTHING IN {example}';
-  translations['!!!'] = 'THEY ARE VARIABLES';
-  return translations;
+class Lang {
+  lang: Language;
+  translations: Record<string, string | undefined>;
+  constructor(lang: Language, mainCheck: boolean = false) {
+    if (mainCheck) console.log(`Checking Translations for ${lang}`);
+    this.lang = lang;
+    this.translations = getTranslations(this.lang);
+    this.translations['!!'] = 'DO NOT TRANSLATE ANYTHING IN {example}';
+    this.translations['!!!'] = 'THEY ARE VARIABLES';
+    if (this.lang === 'en_us') this.injectTimezones();
+    if (this.lang !== 'en_us') this.cleanKeys();
+    if (mainCheck) this.saveTranslations();
+  }
+
+  getTimezones(): string[] {
+    const timezones = Intl.supportedValuesOf('timeZone');
+    timezones.push('UTC');
+    return timezones;
+  }
+
+  injectTimezones() {
+    const timezones = this.getTimezones();
+    timezones.forEach((timezone) => {
+      if (this.translations[`config.options.misc.timezone.${timezone.replaceAll('/', '.')}`] === undefined) {
+        this.translations[`config.options.misc.timezone.${timezone.replaceAll('/', '.')}`] = timezone;
+      }
+    });
+  }
+
+  cleanKeys() {
+    const english = new Lang('en_us');
+    const keys = Object.keys(english.translations);
+    const currentLang = this.translations;
+    this.translations = {};
+    Object.keys(currentLang)
+      .filter((key) => this.getTimezones().includes(key) === false)
+      .filter((key) => !key.startsWith('config.options.misc.timezone.'))
+      .forEach((key) => {
+        if (keys.includes(key)) this.translations[key] = currentLang[key];
+      });
+  }
+
+  saveTranslations() {
+    writeFileSync(`./translations/${this.lang}.json`, JSON.stringify(sortJSON(this.translations), null, 2) + '\n');
+
+    const currentLang = Object.keys(this.translations).filter(
+      (key) => !key.startsWith('config.options.misc.timezone.')
+    );
+    const englishLang = Object.keys(new Lang('en_us').translations).filter(
+      (key) => !key.startsWith('config.options.misc.timezone.')
+    );
+
+    const amount = Math.floor((currentLang.length / englishLang.length) * 100);
+    console.log(`Updated translations for ${this.lang}. (${amount}% tranlated from english)\n`);
+  }
 }
 
-getSupportedLanguages().forEach((lang) => {
-  console.log(`Checking Translations for ${lang}`);
-  const sorted = sortJSON(findTranslations(lang));
-  const timezones = Intl.supportedValuesOf('timeZone');
-  timezones.push('UTC');
-  timezones.forEach((timezone) => {
-    if (sorted[`config.options.misc.timezone.${timezone.replaceAll('/', '.')}`] === undefined) {
-      sorted[`config.options.misc.timezone.${timezone.replaceAll('/', '.')}`] = timezone;
-    }
-  });
-
-  writeFileSync(`./translations/${lang}.json`, JSON.stringify(sortJSON(sorted), null, 2) + '\n');
-});
+getSupportedLanguages().forEach((lang) => new Lang(lang, true));
