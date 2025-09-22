1867e4808d93bb945e1c543b2a10330fd5ddf46e
Jacob <kathund@proton.me>
2025-09-22 16:52:30 +0800

    remove(TimeLeft)

--- src/Utils/TimeAndDateUtils.ts
+++ src/Utils/TimeAndDateUtils.ts
@@ -1,4 +1,3 @@
-import type { TimeLeftData } from '../Types/TimeAndDateUtils.js';
 
 export function getSupportedTimezones(): string[] {
   const timezones = Intl.supportedValuesOf('timeZone');
@@ -13,20 +12,4 @@ export function getUserTimezone(): string {
   if (foundTimezone === undefined) return 'UTC';
   if (getSupportedTimezones().includes(foundTimezone)) return foundTimezone;
   return 'UTC';
-}
-
-export function TimeLeft(unixTimestamp: number): TimeLeftData {
-  const now = Date.now();
-  let diffSeconds = Math.floor(unixTimestamp * 1000 - now) / 1000;
-
-  if (diffSeconds < 0) diffSeconds = 0;
-  const days = Math.floor(diffSeconds / 86400);
-  diffSeconds %= 86400;
-  const hours = Math.floor(diffSeconds / 3600);
-  diffSeconds %= 3600;
-  const minutes = Math.floor(diffSeconds / 60);
-  diffSeconds %= 60;
-  const seconds = Math.floor(diffSeconds);
-
-  return { days, hours, minutes, seconds };
 }
