1f9541b19e607e78f67bf341c5274c737f7c7f50
Jacob <kathund@proton.me>
2025-09-22 08:57:33 +0800

    feat(minecraft-commands): !chocolatefactory

--- src/Config/Configs/MinecraftConfig.ts
+++ src/Config/Configs/MinecraftConfig.ts
@@ -96,6 +96,7 @@ class MinecraftConfig extends ConfigInstance {
           .setValue('boop', minecraftCommandConfig, false)
           .setValue('calculate', minecraftCommandConfig, false)
           .setValue('catacombs', minecraftCommandConfig, false)
+          .setValue('chocolatefactory', minecraftCommandConfig, false)
           .setValue('guild', minecraftCommandConfig, false)
           .setValue('guildexp', minecraftCommandConfig, false)
           .setValue('meow', minecraftCommandConfig, false)
---
+++ src/Minecraft/Commands/ChocolateFactory.ts
@@ -0,0 +1,50 @@
+import Command from '../Private/Command.js';
+import CommandData from '../Private/CommandData.js';
+import CommandDataOption from '../Private/CommandDataOption.js';
+import Translate from '../../Private/Translate.js';
+import { FormatNumber, FormatUsername, ReplaceVariables } from '../../Utils/StringUtils.js';
+import { getLatestProfile } from '../../Utils/HypixelUtils.js';
+import type { MinecraftManagerWithBot } from '../../Types/Minecraft.js';
+
+class ChocolateFactoryCommand extends Command {
+  constructor(minecraft: MinecraftManagerWithBot) {
+    super(minecraft);
+    this.data = new CommandData()
+      .setName('chocolatefactory')
+      .setAliases(['cf', 'factory', 'chocolate'])
+      .setOptions([new CommandDataOption().setName('username').setRequired(false)]);
+  }
+
+  override async execute(player: string, message: string) {
+    try {
+      const args = this.getArgs(message);
+      player = args[0] || player;
+
+      const profile = await getLatestProfile(player);
+      const username = FormatUsername(player, profile.gameMode);
+
+      const { prestige, totalChocolate, currentChocolate, employees } = profile.me.chocolateFactory;
+      const { bro, cousin, sis, father, grandma, dog, uncle } = employees;
+      this.send(
+        ReplaceVariables(Translate('minecraft.commands.chocolatefactory.execute'), {
+          username,
+          prestige,
+          totalChocolate: FormatNumber(totalChocolate),
+          currentChocolate: FormatNumber(currentChocolate),
+          employees,
+          bro,
+          cousin,
+          sis,
+          father,
+          grandma,
+          dog,
+          uncle
+        })
+      );
+    } catch (error) {
+      this.send(`[ERROR] ${error}`);
+    }
+  }
+}
+
+export default ChocolateFactoryCommand;
