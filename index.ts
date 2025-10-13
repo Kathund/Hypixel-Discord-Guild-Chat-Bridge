09c7498d24985c019462d2a4de14e0dc9c64ae9e
Jacob <kathund@proton.me>
2025-10-13 08:36:54 +0800

    fix: dotenv

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
+import 'dotenv/config';
+
 import Application from './src/Application.js';
-import dotenv from 'dotenv';
 import './src/Private/Logger';
 
-dotenv.config();
 new Application().connect();
